5c4f3b30352a409ae35db128bce149100ca84db2
Shalini S <115527635+ShaliniSJ@users.noreply.github.com>
2023-04-28 14:22:37 +0530

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,37 @@
+let cred = JSON.parse(localStorage.getItem('cred')) || {};
+/* use as reference
+let obj = {
+    'shalini' : ['hi','hello']
+}
+let name= 'shalini';
+console.log(obj[`${name}`][1]);
+*/
+console.log(cred);
+function check(){
+    let name= document.querySelector('.js-name').value;
+    let pass= document.querySelector('.js-pass').value;
+    let email = document.querySelector('.js-email').value;
+    //console.log(typeof name,name)
+    if(cred.hasOwnProperty(name)){
+        if(pass === cred[`${name}`][0] && cred[`${name}`][1] === email){
+            window.location.href='index2.html';
+            alert('Welcome back!');
+        }else{
+            alert('Wrong name or password! Try again');
+        }
+    }
+    else{
+        alert('You are not signed up! Please sign up first');
+    }
+    
+}
+function signup(){
+    let name= document.querySelector('.js-name').value;
+    let pass= document.querySelector('.js-pass').value;
+    let email = document.querySelector('.js-email').value;
+    cred[`${name}`] = [pass, email];
+    localStorage.setItem('cred', JSON.stringify(cred));
+    alert('You are signed up!');
+    window.location.href='signin.html';
+
+}
